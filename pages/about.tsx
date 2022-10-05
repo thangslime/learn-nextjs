ad80c37edf6c7f6eb73842917ff6b34229be5226
ThangSlime <thangnguyenanh@tcom.vn>
2022-10-05 10:09:27 +0700

    update

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export interface AboutPageProps {}
+
+export default function AboutPage(props: AboutPageProps) {
+    const css = {
+        r: 255,
+        g: 0,
+        b: 0,
+        a: 0.3
+    }
+    return <div style = {{backgroundColor: `rgba(${css.r},${css.g},${css.b},${css.a})` }}>Hello</div>
+}
